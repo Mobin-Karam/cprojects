82f408a8e36782094e8b499c23742d44a02f1443
mobinkaram <mohammadmobinkaram@gmail.com>
2026-01-27 09:47:14 +0330

    first commit

---
+++ calc.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+int main() {
+	
+	int firstNumber, operator, secondNumber;
+	int sum, multiply, minus, divide;
+	char yesorno;
+	bool repeat = true;
+	printf("***Welcome to my calculator.***\n");
+		
+	do {
+		printf("\tEnter First Number: ");
+		scanf(" %d", &firstNumber);
+	
+		printf("\tEnter Operator: ");
+		scanf(" %c", &operator);
+	
+		printf("\tEnter Second Number: ");
+		scanf(" %d", &secondNumber);
+	
+	
+
+		if (operator == '*') {
+			multiply = firstNumber * secondNumber;
+			printf("Result is: %d\n", multiply);
+		
+		} else if (operator == '+') {
+			sum = firstNumber + secondNumber;
+			printf("Result is: %d\n", sum);
+		
+		} else if (operator == '-') {
+			minus = firstNumber - secondNumber;
+			printf("Result is: %d\n", minus);
+			
+		} else if (operator == '/') {
+			divide = firstNumber / secondNumber;
+			printf("Result is: %d\n", divide);
+		}
+		
+		printf("Do you want to continue (y/n):\n");
+		scanf(" %c", &yesorno);
+		if (yesorno == 'n') {
+			repeat = false;
+			printf(":) come back again ...");
+		}
+		if (!repeat) break;
+	} while (repeat);
+	return 0;
+}
